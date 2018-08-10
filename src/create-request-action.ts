a5ee7ab179de105e7635904274ee99c6391f2b60
Jake 'Sid' Smith <jake@dabapps.com>
2018-08-10 13:12:03 +0100

    createRequestAction

---
+++ src/create-request-action.ts
@@ -0,0 +1,40 @@
+import axios, { AxiosPromise, AxiosRequestConfig } from 'axios';
+import * as deepMerge from 'deepmerge';
+import * as pathToRegexp from 'path-to-regexp';
+import { AnyAction, Dispatch } from 'redux';
+import { ThunkAction } from 'redux-thunk';
+import { RequestActionTypes, RequestOptions } from './types';
+
+export interface StringKeyedObject {[i: string]: any}
+
+export const createRequestAction = <StoreState, Data>
+  (actionTypes: RequestActionTypes, config: AxiosRequestConfig, options: RequestOptions = {}) =>
+    (configOverrides?: AxiosRequestConfig, optionsOverrides: RequestOptions = {}):
+      ThunkAction<AxiosPromise<any>, StoreState, undefined, AnyAction> =>
+      (dispatch: Dispatch) => {
+        const mergedConfig = deepMerge(config, configOverrides);
+        const mergedOptions = deepMerge(options, optionsOverrides);
+
+        const { url } = mergedConfig;
+        const { params } = mergedOptions;
+
+        const resolvedUrl = params ? pathToRegexp.compile(url || '')(params) : url;
+
+        dispatch({type: actionTypes.REQUEST, params});
+
+        return axios
+          .request({
+            ...mergedConfig,
+            url: resolvedUrl,
+          })
+          .then((response) => {
+            dispatch({type: actionTypes.SUCCESS, params, payload: response.data});
+
+            return response.data;
+          })
+          .catch((error) => {
+            dispatch({type: actionTypes.FAILURE, params, payload: error});
+
+            return error;
+          });
+  };
--- src/types.ts
+++ src/types.ts
@@ -3,3 +3,20 @@ export interface RequestActionTypes {
   SUCCESS: symbol;
   FAILURE: symbol;
 }
+
+export interface Params {[i: string]: string | number}
+
+export type RequestOptions = Partial<{
+  params: Params;
+  doNotSendIfExistingRequests: boolean;
+  sendAfterExistingRequests: boolean;
+  abortExistingRequestsOnRequest: boolean;
+  abortExistingRequestsOnSuccess: boolean;
+  abortExistingRequestsOnFailure: boolean;
+  clearDataOnRequest: boolean;
+  clearDataOnSuccess: boolean;
+  clearDataOnFailure: boolean;
+  resetStateOnRequest: boolean;
+  resetStateOnSuccess: boolean;
+  resetStateOnFailure: boolean;
+}>;
