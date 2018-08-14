4bae99e058caa843cf2f57221224b45ddb89c5f1
Jake 'Sid' Smith <jake@dabapps.com>
2018-08-14 11:36:55 +0100

    Index exports

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
 export { createRequestActionTypes } from './create-request-action-types';
+export { createRequestAction } from './create-request-action';
+export { createRequestStateReducer } from './create-request-state-reducer';
+export * from './types';
