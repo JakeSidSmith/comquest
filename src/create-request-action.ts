87b3ea7c3b13e3ba7fb7d27d2bc94ff6c88021ab
Jake 'Sid' Smith <jake@dabapps.com>
2018-08-14 13:22:39 +0100

    Reformat

--- src/create-request-action.ts
+++ src/create-request-action.ts
@@ -5,41 +5,43 @@ import { Dispatch } from 'redux';
 import { ThunkAction } from 'redux-thunk';
 import { ComquestAction, RequestActionTypes, RequestOptions } from './types';
 
-export const createRequestAction = <StoreState, Data, Errors>
-  (actionTypes: RequestActionTypes, config: AxiosRequestConfig, options: RequestOptions = {}) =>
-    (configOverrides: AxiosRequestConfig = {}, optionsOverrides: RequestOptions = {}):
-      ThunkAction<
-        Promise<AxiosResponse<Data> | Errors>,
-        StoreState,
-        undefined,
-        ComquestAction<Data, Errors>
-      > =>
-      (dispatch: Dispatch) => {
-        const mergedConfig = deepMerge<AxiosRequestConfig>(config, configOverrides);
-        const mergedOptions = deepMerge<RequestOptions>(options, optionsOverrides);
+export const createRequestAction = <StoreState, Data, Errors>(
+  actionTypes: RequestActionTypes,
+  config: AxiosRequestConfig,
+  options: RequestOptions = {}
+) =>
+  (
+    configOverrides: AxiosRequestConfig = {},
+    optionsOverrides: RequestOptions = {}
+  ): ThunkAction<Promise<AxiosResponse<Data> | Errors>, StoreState, undefined, ComquestAction<Data, Errors>> =>
+    (
+      dispatch: Dispatch
+    ) => {
+      const mergedConfig = deepMerge<AxiosRequestConfig>(config, configOverrides);
+      const mergedOptions = deepMerge<RequestOptions>(options, optionsOverrides);
 
-        const { url } = mergedConfig;
-        const { params } = mergedOptions;
+      const { url } = mergedConfig;
+      const { params } = mergedOptions;
 
-        const resolvedUrl = params ? pathToRegexp.compile(url || '')(params) : url;
+      const resolvedUrl = params ? pathToRegexp.compile(url || '')(params) : url;
 
-        dispatch({type: actionTypes.REQUEST, options: mergedOptions});
+      dispatch({type: actionTypes.REQUEST, options: mergedOptions});
 
-        return axios
-          .request<Data>({
-            ...mergedConfig,
-            url: resolvedUrl,
-          })
-          .then<AxiosResponse<Data>, Errors>(
-            (response) => {
-              dispatch({type: actionTypes.SUCCESS, payload: response, options: mergedOptions});
+      return axios
+        .request<Data>({
+          ...mergedConfig,
+          url: resolvedUrl,
+        })
+        .then<AxiosResponse<Data>, Errors>(
+          (response) => {
+            dispatch({type: actionTypes.SUCCESS, payload: response, options: mergedOptions});
 
-              return response;
-            },
-            (error) => {
-              dispatch({type: actionTypes.FAILURE, payload: error, options: mergedOptions});
+            return response;
+          },
+          (error) => {
+            dispatch({type: actionTypes.FAILURE, payload: error, options: mergedOptions});
 
-              return error;
-            }
-          );
-  };
+            return error;
+          }
+        );
+};
