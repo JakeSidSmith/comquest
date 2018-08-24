40d0b0c030b8fd33fd8612305ef4ad470f74f393
Jake 'Sid' Smith <jake@dabapps.com>
2018-08-24 19:30:44 +0100

    Test axios request

--- tests/create-request-action.ts
+++ tests/create-request-action.ts
@@ -1,3 +1,6 @@
+import mockAxios from './helpers/mock-axios';
+jest.mock('axios', () => ({default: mockAxios}));
+
 import { createRequestAction, createRequestActionTypes } from '../src';
 
 describe('createRequestAction', () => {
@@ -11,6 +14,33 @@ describe('createRequestAction', () => {
     const result = createRequestAction(actionTypes, {});
 
     expect(typeof result).toBe('function');
+  });
+
+  it('should make an axios request', () => {
+    const dispatch = jest.fn();
+    const getState = jest.fn().mockReturnValue({});
+
+    const actionTypes = createRequestActionTypes('foo');
+    const action = createRequestAction(actionTypes, {});
+
+    expect(mockAxios.calls.length).toBe(0);
+
+    action()(dispatch, getState, undefined);
+
+    const { calls } = mockAxios;
+
+    expect(calls.length).toBe(1);
+
+    const { thenCalls, catchCalls } = calls[0];
+
+    expect(thenCalls.length).toBe(1);
+    expect(catchCalls.length).toBe(0);
+
+    const { arguments: args } = thenCalls[0];
+
+    expect(args.length).toBe(2);
+    expect(typeof args[0]).toBe('function');
+    expect(typeof args[1]).toBe('function');
   });
 
 });
