8b4c144304f857730ccf3afa8930a2d19f598d4b
Jake 'Sid' Smith <jake@dabapps.com>
2018-09-10 21:25:01 +0100

    Swap fat arrow for named function

--- src/create-comquest-action.ts
+++ src/create-comquest-action.ts
@@ -18,68 +18,79 @@ export function createComquestAction<S, D>(
   config: AxiosRequestConfig,
   options: ComquestOptions = {}
 ): ComquestActionCreator<S> {
-  return (configOverrides = {}, optionsOverrides = {}) => dispatch => {
-    const mergedConfig = deepMerge<AxiosRequestConfig>(config, configOverrides);
-    const mergedOptions = deepMerge<ComquestOptions>(options, optionsOverrides);
-
-    const { url = '' } = mergedConfig;
-    const { params } = mergedOptions;
-
-    const resolvedUrl = params ? pathToRegexp.compile(url)(params) : url;
-
-    const source = axios.CancelToken.source();
-
-    const meta = {
-      comquest: COMQUEST_MAGIC_SYMBOL,
-      cancelTokenSource: source,
-      url: resolvedUrl,
-      config: mergedConfig,
-      options: mergedOptions,
-    };
-
-    dispatch({
-      type: actionTypes.REQUEST,
-      meta: { ...meta, type: COMQUEST_REQUEST },
-    });
-
-    return axios
-      .request<D>({
-        ...mergedConfig,
-        cancelToken: source.token,
-        url: resolvedUrl,
-      })
-      .then<AxiosResponse<D>, AxiosError>(
-        (response: AxiosResponse<D>) => {
-          dispatch({
-            type: actionTypes.SUCCESS,
-            payload: response,
-            meta: {
-              ...meta,
-              type: COMQUEST_SUCCESS,
-            },
-          });
-
-          return response;
-        },
-        (error: AxiosError) => {
-          dispatch({
-            type: actionTypes.FAILURE,
-            payload: error,
-            error: true,
-            meta: {
-              ...meta,
-              type: COMQUEST_FAILURE,
-            },
-          });
-
-          if (mergedOptions.throwErrors) {
-            if (!mergedOptions.suppressCancelErrors || !axios.isCancel(error)) {
-              throw error;
-            }
-          }
-
-          return error;
-        }
+  return function comquestAction(configOverrides = {}, optionsOverrides = {}) {
+    return dispatch => {
+      const mergedConfig = deepMerge<AxiosRequestConfig>(
+        config,
+        configOverrides
       );
+      const mergedOptions = deepMerge<ComquestOptions>(
+        options,
+        optionsOverrides
+      );
+
+      const { url = '' } = mergedConfig;
+      const { params } = mergedOptions;
+
+      const resolvedUrl = params ? pathToRegexp.compile(url)(params) : url;
+
+      const source = axios.CancelToken.source();
+
+      const meta = {
+        comquest: COMQUEST_MAGIC_SYMBOL,
+        cancelTokenSource: source,
+        url: resolvedUrl,
+        config: mergedConfig,
+        options: mergedOptions,
+      };
+
+      dispatch({
+        type: actionTypes.REQUEST,
+        meta: { ...meta, type: COMQUEST_REQUEST },
+      });
+
+      return axios
+        .request<D>({
+          ...mergedConfig,
+          cancelToken: source.token,
+          url: resolvedUrl,
+        })
+        .then<AxiosResponse<D>, AxiosError>(
+          (response: AxiosResponse<D>) => {
+            dispatch({
+              type: actionTypes.SUCCESS,
+              payload: response,
+              meta: {
+                ...meta,
+                type: COMQUEST_SUCCESS,
+              },
+            });
+
+            return response;
+          },
+          (error: AxiosError) => {
+            dispatch({
+              type: actionTypes.FAILURE,
+              payload: error,
+              error: true,
+              meta: {
+                ...meta,
+                type: COMQUEST_FAILURE,
+              },
+            });
+
+            if (mergedOptions.throwErrors) {
+              if (
+                !mergedOptions.suppressCancelErrors ||
+                !axios.isCancel(error)
+              ) {
+                throw error;
+              }
+            }
+
+            return error;
+          }
+        );
+    };
   };
 }
