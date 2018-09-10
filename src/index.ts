2ff33e40bf091f4a2dcc83eaa2bca8a48dace2df
Jake 'Sid' Smith <jake@dabapps.com>
2018-09-10 20:08:50 +0100

    Rename files

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-export { createRequestActionTypes } from './create-request-action-types';
-export { createRequestAction } from './create-request-action';
-export { createRequestDataReducer } from './create-request-data-reducer';
-export { createRequestErrorReducer } from './create-request-error-reducer';
-export { createRequestStateReducer } from './create-request-state-reducer';
-export { composeReducers } from './compose-reducers';
+export { createRequestActionTypes } from './create-comquest-action-types';
+export { createRequestAction } from './create-comquest-action';
+export { createRequestDataReducer } from './create-comquest-data-reducer';
+export { createRequestErrorReducer } from './create-comquest-error-reducer';
+export { createRequestStateReducer } from './create-comquest-state-reducer';
+export { composeReducers } from './compose-comquest-reducers';
 export * from './types';
