db2b9ef483074584ee5aefb320f8cdfe85c463be
Jake 'Sid' Smith <jake@dabapps.com>
2018-09-11 13:02:54 +0100

    Setup store with middleware for testing

--- tests/create-comquest-middleware.ts
+++ tests/create-comquest-middleware.ts
@@ -1,4 +1,11 @@
-import { createComquestMiddleware } from '../src';
+import { AxiosError, AxiosResponse } from 'axios';
+import { applyMiddleware, createStore } from 'redux';
+import {
+  createComquestActionTypes,
+  createComquestMiddleware,
+  createComquestRequestDataReducer,
+  createComquestRequestErrorReducer,
+} from '../src';
 
 describe('createComquestMiddleware', () => {
   it('returns a middleware', () => {
@@ -14,5 +21,39 @@ describe('createComquestMiddleware', () => {
     expect(typeof afterNext).toBe('function');
     const afterAction = afterNext(action);
     expect(afterAction).toBe(action);
+  });
+
+  const actionTypes = createComquestActionTypes('test');
+  const dataReducer = createComquestRequestDataReducer(actionTypes);
+  const errorReducer = createComquestRequestErrorReducer(actionTypes);
+
+  const transformRequestData = jest
+    .fn()
+    .mockImplementation((value: AxiosResponse) => {
+      return value.data;
+    });
+
+  const transformRequestError = jest
+    .fn()
+    .mockImplementation((value: AxiosError) => {
+      return typeof value.response !== 'undefined'
+        ? value.response.data
+        : value.message;
+    });
+
+  describe('transformRequestData', () => {
+    const middleware = createComquestMiddleware({ transformRequestData });
+    const store = createStore(dataReducer, applyMiddleware(middleware));
+    it('should transform all success request data', () => {
+      expect(store.getState()).toEqual({});
+    });
+  });
+
+  describe('transformRequestError', () => {
+    const middleware = createComquestMiddleware({ transformRequestError });
+    const store = createStore(errorReducer, applyMiddleware(middleware));
+    it('should transform all failure request errors', () => {
+      expect(store.getState()).toEqual({});
+    });
   });
 });
