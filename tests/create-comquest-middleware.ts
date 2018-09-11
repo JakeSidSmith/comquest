835c5b5d7a065c23d936ad3062c476a6620db8d6
Jake 'Sid' Smith <jake@dabapps.com>
2018-09-11 13:15:37 +0100

    Test transforming data and errors with middleware

--- tests/create-comquest-middleware.ts
+++ tests/create-comquest-middleware.ts
@@ -1,6 +1,10 @@
+import mockAxios from './helpers/mock-axios';
+jest.mock('axios', () => ({ default: mockAxios }));
+
 import { AxiosError, AxiosResponse } from 'axios';
 import { applyMiddleware, createStore } from 'redux';
 import {
+  createComquestAction,
   createComquestActionTypes,
   createComquestMiddleware,
   createComquestRequestDataReducer,
@@ -8,10 +12,14 @@ import {
 } from '../src';
 
 describe('createComquestMiddleware', () => {
+  beforeEach(() => {
+    mockAxios.clear();
+  });
+
   it('returns a middleware', () => {
     const store = { dispatch: jest.fn(), getState: jest.fn() };
     const next = jest.fn().mockImplementation((value: any) => value);
-    const action = { type: 'unknown' };
+    const unknownAction = { type: 'unknown' };
     const middleware = createComquestMiddleware({});
 
     expect(typeof middleware).toBe('function');
@@ -19,11 +27,12 @@ describe('createComquestMiddleware', () => {
     expect(typeof afterStore).toBe('function');
     const afterNext = afterStore(next);
     expect(typeof afterNext).toBe('function');
-    const afterAction = afterNext(action);
-    expect(afterAction).toBe(action);
+    const afterAction = afterNext(unknownAction);
+    expect(afterAction).toBe(unknownAction);
   });
 
   const actionTypes = createComquestActionTypes('test');
+  const action = createComquestAction(actionTypes, {});
   const dataReducer = createComquestRequestDataReducer(actionTypes);
   const errorReducer = createComquestRequestErrorReducer(actionTypes);
 
@@ -46,6 +55,28 @@ describe('createComquestMiddleware', () => {
     const store = createStore(dataReducer, applyMiddleware(middleware));
     it('should transform all success request data', () => {
       expect(store.getState()).toEqual({});
+
+      action()(store.dispatch, store.getState, undefined);
+
+      expect(store.getState()).toEqual({});
+
+      const { requestCalls } = mockAxios;
+
+      expect(requestCalls.length).toBe(1);
+
+      const { thenCalls } = requestCalls[0];
+
+      expect(thenCalls.length).toBe(1);
+
+      const [success] = thenCalls[0].arguments;
+
+      success({
+        data: {
+          foo: 'bar',
+        },
+      });
+
+      expect(store.getState()).toEqual({ data: { foo: 'bar' } });
     });
   });
 
@@ -54,6 +85,28 @@ describe('createComquestMiddleware', () => {
     const store = createStore(errorReducer, applyMiddleware(middleware));
     it('should transform all failure request errors', () => {
       expect(store.getState()).toEqual({});
+
+      action()(store.dispatch, store.getState, undefined);
+
+      expect(store.getState()).toEqual({});
+
+      const { requestCalls } = mockAxios;
+
+      expect(requestCalls.length).toBe(1);
+
+      const { thenCalls } = requestCalls[0];
+
+      expect(thenCalls.length).toBe(1);
+
+      const [, failure] = thenCalls[0].arguments;
+
+      failure(new Error('error'));
+
+      expect(store.getState()).toEqual({ error: 'error' });
+
+      failure({ response: { data: 'error response' } });
+
+      expect(store.getState()).toEqual({ error: 'error response' });
     });
   });
 });
