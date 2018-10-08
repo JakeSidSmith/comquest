1515a9b114b9293ff261f4eea36925b35b4c4385
Jake 'Sid' Smith <jake@dabapps.com>
2018-10-08 21:55:15 +0100

    Add basic test for endpoint

---
+++ tests/create-comquest-api-endpoint.ts
@@ -0,0 +1,26 @@
+import { createComquestActionTypes, createComquestAPIEndpoint } from '../src';
+
+describe('createComquestAPIEndpoint', () => {
+  const actionTypes = createComquestActionTypes('TEST');
+
+  it('should create an object with all HTTP methods', () => {
+    const endpoint = createComquestAPIEndpoint(actionTypes, {}, {});
+    const endpointKeys = Object.keys(endpoint) as Array<keyof typeof endpoint>;
+
+    expect(endpointKeys).toEqual([
+      'get',
+      'post',
+      'put',
+      'head',
+      'delete',
+      'patch',
+      'options',
+    ]);
+
+    endpointKeys.forEach(endpointKey => {
+      const method = endpoint[endpointKey];
+
+      expect(typeof method).toBe('function');
+    });
+  });
+});
